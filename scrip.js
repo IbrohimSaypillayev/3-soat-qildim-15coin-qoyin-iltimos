fdfee625ce1421609585ea86de795fff20e99f9d
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-03-13 19:00:47 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,10 @@
+let ismingiz = prompt("sizning typedagi malomot kiriting")
+
+let yoshingiz = prompt("number typedagi malumotingizni kiriting")
+
+let alo = prompt("bolean typedagimalomotni kiriting")
+
+
+alert("hamma malumot togri")
+
+alert(`siz kiritgan ${ ismingiz} string typega tegizshli,siz kiritkan${ yoshingiz } number typga tegishli,siz kiritgan ${alo}sozi boolen typega tegishli`)
